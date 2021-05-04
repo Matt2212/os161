da896d9f055aa65014beba6d8751e3c14672c9de
Mattia <mattiacaracciolo1@gmail.com>
2021-05-04 02:13:42 +0200

    added syscalls

---
+++ os161-base-2.0.3/kern/syscall/file_syscalls.c
@@ -0,0 +1,25 @@
+#include <syscall.h>
+#include <sys/types.h>
+#include <lib.h>
+//#include <types/size_t.h>
+ssize_t sys_write(int fd, const void *buff, size_t count){
+	
+	(void) fd;
+	char* buf = (char*) buff;
+	int i = 0;
+	while(buff[i] != 0 && i < (int)count)
+		putch(buff[i++]);
+	return i;
+}
+
+ssize_t sys_read(int fd, void *buff, size_t count) {
+	(void) fd;
+	int i = 0;
+	char* buf = (char*) buff;
+        while(i < (int)count) {
+        	buff[i] = getch();
+		if (buff[i] < 0) return i;
+		i++;
+	}
+	return i;
+}
